5d7486d3f24dbae257c12ebff9606b6869623791
jose F Boccalandro <jofebocc@gmail.com>
2019-09-16 12:02:24 -0500

    Project1_1
    open File works with .txt files

--- project_1_0/src/application/app.java
+++ project_1_0/src/application/app.java
@@ -8,11 +8,11 @@ import java.awt.event.*;
 import java.io.*;
 
 public class app {
-    private JTextField textField1;
     private JPanel textEditor;
     private JButton button1;
     private JButton Create;
     private JButton saveProjectButton;
+    private JTextArea textArea1;
 
 
     public app() {
@@ -20,15 +20,27 @@ public class app {
             @Override
             public void actionPerformed(ActionEvent e) {
                 final JFileChooser fc = new JFileChooser();
-                fc.setCurrentDirectory(new java.io.File("."));
-                fc.setDialogTitle("Files");
-                fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-                if (fc.showOpenDialog(button1) == JFileChooser.APPROVE_OPTION){
+//                fc.setCurrentDirectory(new java.io.File("."));
+//                fc.setDialogTitle("Files");
+//                fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                fc.showOpenDialog(null);
+                File f = fc.getSelectedFile();
+                String filename = f.getAbsolutePath();
+                    try{
+                        FileReader reader = new FileReader(filename);
+                        BufferedReader br = new BufferedReader(reader);
+                        textArea1.read(br,null);
+                        br.close();
+                        textArea1.requestFocus();
+                    } catch (FileNotFoundException ex) {
+                        ex.printStackTrace();
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
 
-                }
             }
         });
-        saveProjectButton.addActionListener(new ActionListener() {
+        Create.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 JFileChooser fs = new JFileChooser();
@@ -43,11 +55,12 @@ public class app {
 
             }
         });
-        Create.addActionListener(new ActionListener() {
+        saveProjectButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String txt = textField1.getText();
+                String txt = textArea1.getText();
                 JFileChooser n = new JFileChooser(".");
+                n.setDialogTitle("Create");
                 int retval = n.showSaveDialog(null);
                 if (retval == JFileChooser.APPROVE_OPTION){
                     File file = n.getSelectedFile();
@@ -58,7 +71,7 @@ public class app {
                         file = new File(file.getParentFile(), file.getName() + ".txt");
                     }
                     try {
-                        textField1.write(new OutputStreamWriter(new FileOutputStream(file) , "utf-8"));
+                        textArea1.write(new OutputStreamWriter(new FileOutputStream(file) , "utf-8"));
                         Desktop.getDesktop().open(file);
                     } catch (FileNotFoundException ex) {
                         ex.printStackTrace();
