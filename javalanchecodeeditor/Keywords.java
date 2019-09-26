179b297a771e2af0237e73fe3dfa7309308782e5
dwijap <dwijaparikh@gmail.com>
2019-09-26 16:24:24 -0500

    added keyword support

---
+++ javalanchecodeeditor/Keywords.java
@@ -0,0 +1,16 @@
+package javalanchecodeeditor;
+
+import java.lang.String;
+
+public class Keywords {
+    private String[] keywords = {"abstract", "assert", "boolean",
+            "break", "byte", "case", "catch", "char", "class", "const",
+            "continue", "default", "do", "double", "else", "extends", "false",
+            "final", "finally", "float", "for", "goto", "if", "implements",
+            "import", "instanceof", "int", "System", "out", "print()", "println()",
+            "new", "null", "package", "private", "protected", "public", "interface",
+            "long", "native", "return", "short", "static", "strictfp", "super", "switch",
+            "synchronized", "this", "throw", "throws", "transient", "true",
+            "try", "void", "volatile", "while", "String"};
+    private String[] operators = { "+", "-", "*", "/", "//"};
+}
