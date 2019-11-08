50ae83bf5b18d8dd5ab5d94cd3085af63388d788
Paola Garibay <paolagaribay55@gmail.com>
2019-11-07 18:16:21 -0600

    Update KeywordCounter

--- javalanchecodeeditor/KeywordCounter.java
+++ javalanchecodeeditor/KeywordCounter.java
@@ -1,40 +1,48 @@
-package javalanchecodeeditor;
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
-public class KeywordCounter {
-	//attr
-	private int keywords;
-	//constructor
-	KeywordCounter() {keywords = 0;}
-	//methods
-	int countKeywords(File f) throws IOException {
-		int numkeys = 0;
-		int search;
-		boolean commented = false;
-		String line = new String();
-		Scanner sc = new Scanner(f);
-		while(sc.hasNextLine()) {
-			line = sc.nextLine();
-			if(line.contains("/*")) {
-				commented = true;
-			}
-			if(!commented) {
-				if(line.contains("//")) {
-					if(line.indexOf("//") != 0) {
-						line = line.substring(0, line.indexOf("//") - 1);
-					}
-				}
-				if(line.contains("if") || line.contains("else") || line.contains("while") || line.contains("for")) {
-					numkeys++;
-				}
-			}
-			if(line.contains("*/")) {
-				commented = false;
-			}
 
-		}
-		
-		return numkeys;
-	}
-	
+class KeywordCounter {
+
+    private static String[] JAVA_KEYWORDS = new String[]{"String",
+            "System", "abstract", "assert", "boolean", "break", "byte", "case",
+            "catch", "char", "class", "const", "continue", "default", "do",
+            "double", "else", "enum", "extends", "false", "final",
+            "finally", "float", "for", "goto", "if", "implements",
+            "import", "instanceof", "int", "interface", "long", "native",
+            "new", "null", "package", "print()",
+            "println()", "private", "protected", "public", "return", "short",
+            "static", "strictfp", "super", "switch", "synchronized", "this", "throw",
+            "throws", "transient", "true", "try", "void",
+            "volatile", "while"};
+
+    static int countKeywords(File f) throws IOException {
+        int numkeys = 0;
+        boolean commented = false;
+        String line;
+        Scanner sc = new Scanner(f);
+        while(sc.hasNextLine()) {
+            line = sc.nextLine();
+            if(line.contains("/*")) {
+                commented = true;
+            }
+            if(!commented) {
+                for(String i: line.split("\\s+")) {
+                    if(i.equals("//")) {
+                        break;
+                    }
+                    for(String w: JAVA_KEYWORDS) {
+                        if(i.equals(w)) {
+                            numkeys++;
+                        }
+                    }
+                }
+            }
+            if(line.contains("*/")) {
+                commented = false;
+            }
+
+        }
+        return numkeys;
+    }
 }
