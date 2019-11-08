2b98395720ca4d9107fa5f5718894e6132788c26
Paola Garibay <paolagaribay55@gmail.com>
2019-11-07 18:27:42 -0600

    Update KeywordCounter

--- javalanchecodeeditor/KeywordCounter.java
+++ javalanchecodeeditor/KeywordCounter.java
@@ -10,8 +10,7 @@ class KeywordCounter {
             "double", "else", "enum", "extends", "false", "final",
             "finally", "float", "for", "goto", "if", "implements",
             "import", "instanceof", "int", "interface", "long", "native",
-            "new", "null", "package", "print()",
-            "println()", "private", "protected", "public", "return", "short",
+            "new", "null", "package", "private", "protected", "public", "return", "short",
             "static", "strictfp", "super", "switch", "synchronized", "this", "throw",
             "throws", "transient", "true", "try", "void",
             "volatile", "while"};
